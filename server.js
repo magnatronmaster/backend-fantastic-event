b17b6e810fe00afcb307f8363e400ff2dd95d738
Julian Builes <julianbuilesc@gmail.com>
2020-08-20 23:05:38 -0500

    Add endpoint user

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const sponsors = require('./routes/sponsors');
+const users = require('./routes/users');
 const { config } = require('./config');
-
-//Conection DB
-const MysqlLib = require('./lib/mysql')
-const mysqlLib= new MysqlLib();
 
 const app = express();
 // body parser
@@ -14,6 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // routes
 sponsors(app);
+users(app);
 
 app.listen(config.port, () => {
   console.log(`Servidor activo en http://localhost:${config.port}`);
