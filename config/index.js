a6b47bad0491f112269a740841acbb7c5c69df01
Julian Builes <julianbuilesc@gmail.com>
2020-08-20 23:04:43 -0500

    add envairoment port

--- config/index.js
+++ config/index.js
@@ -4,9 +4,10 @@ const config = {
   port: process.env.PORT || 3000,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
-  dbName : process.env.DB_NAME,
-  dbHost : process.env.DB_HOST,
-  dbDialect : process.env.DB_DIALECT,
+  dbName: process.env.DB_NAME,
+  dbHost: process.env.DB_HOST,
+  dbDialect: process.env.DB_DIALECT,
+  dbPort: process.env.DB_PORT,
 };
 
 module.exports = { config };
