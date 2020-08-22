07114538089fff79e56e3eeb79073e4e158997ec
Luis LC <luis.lazcanocruz@gmail.com>
2020-08-22 17:25:21 -0500

    Events model with migration

--- migration.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-const sequelize = require('./lib/repository/connection');
-const Sponsor = require('./lib/models/sponsors');
-const sponsor = Sponsor(sequelize, Sequelize);
-
-
-sequelize.sync({ force: true }).then(() => {
-  console.log(`Database & tables created!`);
-});
