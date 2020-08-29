b94d3c386abbf9673d87b38be6452d27e0ccc4d3
Julian Builes <julianbuilesc@gmail.com>
2020-08-29 18:53:34 -0500

    add field name_register

--- utils/schemas/register.js
+++ utils/schemas/register.js
@@ -2,6 +2,7 @@ const Joi = require('joi');
 const { id_eventSchema, idEventSchema } = require('./event');
 const createRegisterSchema = Joi.object({
   email_register: Joi.string().email().required(),
+  name_register: Joi.string().min(3).max(100).required(),
   id_event: id_eventSchema.required(),
 });
 
