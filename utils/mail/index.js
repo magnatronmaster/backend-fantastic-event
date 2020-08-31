4304e6f899fa49a8b7c40b164876f879c3629b48
Luis LC <luis.lazcanocruz@gmail.com>
2020-08-30 19:08:04 -0500

    Ading dates to events mail

--- utils/mail/index.js
+++ utils/mail/index.js
@@ -1,4 +1,4 @@
-const { format } = require('date-fns')
+const { format, add } = require('date-fns')
 const email = require('./email');
 const EventService = require('../../services/event')
 const eventService= new EventService();
@@ -14,14 +14,12 @@ function sendEmail(){
 //   );
 
 //   console.log('Message sent: %s', info.messageId);
-  // const today = format(endOfDay(new Date()).toISOString());
-  // const today= format(new Date(2020, 30, 08), 'yyyy-MM-dd') + 'T23:59:59.999Z';
-  // console.log('Here init');
-  // console.log(today)
-  // console.log('Here end');
+  const today = format(new Date(), 'yyyy-MM-dd');
+  const afterTomorrow = format(add(new Date(), { days: 2 }), 'yyyy-MM-dd')
+
   const events = eventService.getEventsMail({
     date_start_event: {
-      [Op.between]: ['2020-08-30', '2020-09-1']
+      [Op.between]: [ today, afterTomorrow ]
     }
   });
   // events.map((event) => {
